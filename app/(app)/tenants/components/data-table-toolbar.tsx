aa954eb4696db08b760a3d0bc46813503aa76880
patwikx <patricklacapmiranda@gmail.com>
2024-03-30 18:06:36 +0800

    fixed errors

--- app/(app)/tenants/components/data-table-toolbar.tsx
+++ app/(app)/tenants/components/data-table-toolbar.tsx
@@ -5,7 +5,7 @@ import { Table } from "@tanstack/react-table"
 
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { DataTableViewOptions } from "@/app/(app)/taskmanager/components/data-table-view-options"
+import { DataTableViewOptions } from '../components/data-table-view-options'
 
 import { statuses } from "../data/data"
 import { DataTableFacetedFilter } from "./data-table-faceted-filter"
