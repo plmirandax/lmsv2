06fe6a2b72447bdd19133658545563804e5fc976
patwikx <patricklacapmiranda@gmail.com>
2024-04-24 18:26:16 +0800

    removed

--- components/forms/changePwd.tsx
+++ components/forms/changePwd.tsx
@@ -1,10 +1,6 @@
 'use client'
 
 import { Button } from "@/components/ui/button"
-import {
-  Dialog,
-  DialogContent,
-} from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../../components/ui/card"
